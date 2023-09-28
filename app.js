c5b0a232490bf7372c9ce63e960368b9ac394205
anugrahwd <nurisnainianugrah01@gmail.com>
2023-09-28 20:43:25 +0800

    update app

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+
+const connection = require('./config/mysql.js')
+const hospitalRouter = require('./routes/hospitalRoute.js')
 
 dotenv.config()
 
-const app = express()
-
+const app = express();
 const port = process.env.PORT || 5000;
+
+connection.connect()
+
+app.use(express.json());
+app.use(hospitalRouter)
 
 app.listen(port, function() {
     console.log("server running on", port)
